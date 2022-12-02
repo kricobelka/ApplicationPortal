c620e50426e38e9db4e819f5b2d45baa2f9078fd
Hanna Ivanova <hijikataanya@gmail.com>
2022-12-02 22:35:24 +0300

    test

--- ApplicationPortal/ApplicationPortal/Controllers/HomeController.cs
+++ ApplicationPortal/ApplicationPortal/Controllers/HomeController.cs
@@ -48,5 +48,6 @@ namespace ApplicationPortal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
