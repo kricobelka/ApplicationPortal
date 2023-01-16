ac56c5cc8ff099027385c50b897dd267666b015c
Hanna Ivanova <hijikataanya@gmail.com>
2023-01-17 01:51:28 +0300

    Login-Register-Views-Added

--- ApplicationPortal/ApplicationPortal/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ApplicationPortal/ApplicationPortal/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -146,15 +146,8 @@ namespace ApplicationPortal.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index","Home");
                 }
                 foreach (var error in result.Errors)
                 {
@@ -208,7 +201,8 @@ namespace ApplicationPortal.Areas.Identity.Pages.Account
 
             var comp = await _context.Companies.Select(q => new SelectListItem()
             {
-                Text = q.CompanyName, Value = q.CompanyId.ToString(),
+                Text = q.CompanyName,
+                Value = q.CompanyId.ToString(),
             }).ToListAsync();
 
             //ViewBag.SelectedList = comp;
